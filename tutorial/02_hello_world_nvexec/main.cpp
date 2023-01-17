04fd1b1f78079c09cd9e9f078c1f5b43542aeed5
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2023-01-17 12:43:17 +0900

    Add hello world in nvexec

---
+++ tutorial/02_hello_world_nvexec/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdexec/execution.hpp>
+#include "nvexec/stream_context.cuh"
+
+using stdexec::just; using stdexec::on;
+using stdexec::sender; using stdexec::schedule;
+using stdexec::then; using stdexec::sync_wait;
+using stdexec::run_loop; using stdexec::get_scheduler;
+using stdexec::let_value; using stdexec::when_all;
+using stdexec::get_stop_token;
+
+int main(int argc, char *argv[]) {
+  // Declare a CUDA stream
+  nvexec::stream_context stream_ctx{};
+
+  auto sched = stream_ctx.get_scheduler();
+  sender auto begin = schedule(sched);
+  sender auto hi_again = then(begin, [] {
+    return 13;
+  });
+
+  sender auto add_42 = then(hi_again, [](int arg) {return arg + 42;});
+  auto [i] = sync_wait(std::move(add_42)).value();
+  std::cout << "Result: " << i << std::endl;
+
+  // Sync_wait provides a run_loop scheduler
+  std::tuple<run_loop::__scheduler> t =
+    sync_wait(get_scheduler()).value();
+  (void) t;
+  
+  auto y = let_value(get_scheduler(), [](auto sched){
+    return on(sched, then(just(), []{std::cout << "from run_loop\n";return 42;}));
+  });
+  sync_wait(std::move(y));
+  
+  sync_wait(when_all(just(42), get_scheduler(), get_stop_token()));
+}
