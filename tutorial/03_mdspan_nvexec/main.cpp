b1d47b4986f7dc51b12d33a32eddf89c102aba74
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2023-01-25 18:09:53 +0900

    Add mdspan bulk example

--- tutorial/03_mdspan_nvexec/main.cpp
+++ tutorial/03_mdspan_nvexec/main.cpp
@@ -1,38 +1,38 @@
 #include <iostream>
 #include <stdexec/execution.hpp>
 #include <experimental/mdspan>
+#include <thrust/device_vector.h>
 #include "nvexec/stream_context.cuh"
+#include "exec/on.hpp"
 
 using stdexec::just; using stdexec::on;
 using stdexec::sender; using stdexec::schedule;
 using stdexec::then; using stdexec::sync_wait;
 using stdexec::run_loop; using stdexec::get_scheduler;
 using stdexec::let_value; using stdexec::when_all;
-using stdexec::get_stop_token;
+using stdexec::get_stop_token; using stdexec::bulk;
+namespace stdex = std::experimental;
 
 int main(int argc, char *argv[]) {
   // Declare a CUDA stream
   nvexec::stream_context stream_ctx{};
-
   auto sched = stream_ctx.get_scheduler();
-  sender auto begin = schedule(sched);
-  sender auto hi_again = then(begin, [] {
-    return 13;
-  });
 
-  sender auto add_42 = then(hi_again, [](int arg) {return arg + 42;});
-  auto [i] = sync_wait(std::move(add_42)).value();
-  std::cout << "Result: " << i << std::endl;
+  const std::size_t nx = 3, ny = 2;
+  const std::size_t n = nx * ny;
+  thrust::device_vector<std::size_t> a(n, 1), b(n, 2);
+  stdex::mdspan<double, stdex::extents<std::size_t, ny, nx>> dst((double *)thrust::raw_pointer_cast(a.data()));
+  stdex::mdspan<double, stdex::extents<std::size_t, ny, nx>> src((double *)thrust::raw_pointer_cast(b.data()));
 
-  // Sync_wait provides a run_loop scheduler
-  std::tuple<run_loop::__scheduler> t =
-    sync_wait(get_scheduler()).value();
-  (void) t;
-  
-  auto y = let_value(get_scheduler(), [](auto sched){
-    return on(sched, then(just(), []{std::cout << "from run_loop\n";return 42;}));
-  });
-  sync_wait(std::move(y));
-  
-  sync_wait(when_all(just(42), get_scheduler(), get_stop_token()));
+  auto copy_kernel = [dst, src](const std::size_t idx) {
+    std::size_t ix = idx % nx;
+    std::size_t iy = idx / nx;
+    dst(ix, iy) = src(ix, iy);
+  };
+
+  auto init = just()
+            | exec::on(sched, bulk(n, copy_kernel)); 
+  stdexec::sync_wait(init);
+
+  return 0;
 }
