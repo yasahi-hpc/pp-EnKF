f1727d777d8ac883d894d98a4a987fe57045bc07
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2023-02-03 15:45:11 +0900

    Add stream bandwidth measurement

--- wk/sub_aquarius.sh
+++ wk/sub_aquarius.sh
@@ -18,4 +18,5 @@ spack load gcc@11.3.0
 spack load cmake@3.24.3
 module load /work/jh220031a/i18048/lib/nvidia/hpc_sdk22.11/modulefiles/nvhpc/22.11
 
-../build/tutorial/02_hello_world_nvexec/02_hello_world
+../build/tutorial/04_stream/04_stream_test
+#../build/tutorial/02_hello_world_nvexec/02_hello_world
